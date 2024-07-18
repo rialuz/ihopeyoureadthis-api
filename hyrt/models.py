50cd5e37a916bd1e74679111b3e8818ac05c48b3
Luz Lopez Marcano <l.lopezmarcano4@gmail.com>
2024-07-17 20:04:03 -0400

    updated created_at field

--- hyrt/models.py
+++ hyrt/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Letter(models.Model):
     sender = models.CharField(max_length=100, blank=True)
     recipient = models.CharField(max_length=100)
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     message = models.TextField()
 
 
